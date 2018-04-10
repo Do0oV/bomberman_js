b85ade79fceef6061bee894b72079599658aa805
Do0oV <viard.dorothee@gmail.com>
2018-04-10 15:07:58 +0200

    added random enemy moves

--- js/main.js
+++ js/main.js
@@ -3,26 +3,19 @@ let hero = document.getElementById('hero');
 let bomb = document.getElementById('bomb');
 let controlActive = true;
 
-
+// HERO MOVES//
 document.addEventListener("keydown", function(e){
 
     let heroLeft = hero.offsetLeft;
     let heroTop = hero.offsetTop;
     let posBlockLeft = hero.offsetLeft / 50;
     let posBlockTop = hero.offsetTop / 50;
-    /*console.log('up ' + grille[posBlockTop - 1][posBlockLeft]);
-    console.log('right ' +grille[posBlockTop][posBlockLeft + 1]);
-    console.log('down ' + grille[posBlockTop + 1][posBlockLeft]);
-    console.log('left ' + grille[posBlockTop][posBlockLeft - 1]);*/
-
 
     if (controlActive) {
-        //console.log('isactive');
 
         switch (e.keyCode) {
         //UP
         case 38:
-        //console.log('up');
         if(grille[posBlockTop - 1][posBlockLeft] === 0){
             hero.style.top = (posBlockTop - 1) * 50 + "px";
             hero.style.backgroundImage = "url('./images/hero_back.png')";
@@ -31,7 +24,6 @@ document.addEventListener("keydown", function(e){
         break;
         //RIGHT
         case 39:
-        //console.log('right');
         if(grille[posBlockTop][posBlockLeft + 1] === 0){
             hero.style.left = (posBlockLeft + 1) * 50 + "px";
             hero.style.backgroundImage = "url('./images/hero_right.png')";
@@ -39,7 +31,6 @@ document.addEventListener("keydown", function(e){
         break;
         //DOWN
         case 40:
-        //console.log('down');
         if(grille[posBlockTop + 1][posBlockLeft] === 0){
             hero.style.top = (posBlockTop + 1) * 50 + "px";
             hero.style.backgroundImage = "url('./images/hero_front.png')";
@@ -48,22 +39,58 @@ document.addEventListener("keydown", function(e){
         break;
         //LEFT
         case 37:
-        //console.log('left');
         if(grille[posBlockTop][posBlockLeft - 1] === 0){
             hero.style.left = (posBlockLeft - 1) * 50 + "px";
             hero.style.backgroundImage = "url('./images/hero_left.png')";
         }
 
         break;
-                //BOMB
-                case 32:
-                //console.log('bomb');
-                bomb.style.left = heroLeft + "px";
-                bomb.style.top = heroTop + "px";
-                bomb.style.backgroundImage = "url('./images/bomb.png')";
-                bomb.style.display = "block";
+        //BOMB
+        case 32:
+        bomb.style.left = heroLeft + "px";
+        bomb.style.top = heroTop + "px";
+        bomb.style.backgroundImage = "url('./images/bomb.png')";
+        bomb.style.display = "block";
 
-                break;
-            }
+        break;
+    }
+}
+});
+
+// ENEMY MOVES //
+
+let enemy = document.getElementById('enemy');
+random();
+setInterval(random, 300);
+
+function random (){
+
+    direction = Math.floor((Math.random() * 4) + 1);
+    let posBlockLeft = enemy.offsetLeft / 50;
+    let posBlockTop = enemy.offsetTop / 50;
+    if(direction == 1 ){
+        if(grille[posBlockTop - 1][posBlockLeft] === 0){
+            enemy.style.top = (posBlockTop - 1) * 50 + "px";
+            enemy.style.backgroundImage = "url('./images/enemy_back.png')";
         }
-    });
+    }else if(direction == 2 ){
+        if(grille[posBlockTop][posBlockLeft + 1] === 0){
+            enemy.style.left = (posBlockLeft + 1) * 50 + "px";
+            enemy.style.backgroundImage = "url('./images/enemy_right.png')";
+        }
+    }else if(direction == 3 ){
+        if(grille[posBlockTop + 1][posBlockLeft] === 0){
+            enemy.style.top = (posBlockTop + 1) * 50 + "px";
+            enemy.style.backgroundImage = "url('./images/enemy_front.png')";
+        }
+    }else if(direction == 4 ){
+        if(grille[posBlockTop][posBlockLeft - 1] === 0){
+            enemy.style.left = (posBlockLeft - 1) * 50 + "px";
+            enemy.style.backgroundImage = "url('./images/enemy_left.png')";
+        }
+    }
+}
+
+
+
+
