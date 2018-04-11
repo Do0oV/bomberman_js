23a21018d51a89e9be4c7a257503ae35509795c7
Do0oV <viard.dorothee@gmail.com>
2018-04-11 12:44:29 +0200

    fixed explosions and changed bricks to floors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 let hero = document.getElementById('hero');
 let bomb = document.getElementById('bomb');
+let brick = document.getElementsByClassName('brick');
 let controlActive = true;
 
 // HERO MOVES//
@@ -96,31 +97,111 @@ function random (){
 
 // EXPLOSION //
 
+
+// Supprime les explosions
 function removeExplosion() {
 
     bomb.style.display = "none";
-}
+    let explo = document.getElementsByClassName('explosion');
 
+    for (var i = explo.length - 1; i >= 0; i--) {
+        explo[i].parentElement.removeChild(explo[i]);
+    }
+}
+// creer les explosions si les cases sont breakable
 function explode() {
+
     let posBombLeft = bomb.offsetLeft / 50;
     let posBombTop = bomb.offsetTop / 50;
+    let down = grille[posBombTop + 1][posBombLeft];
+    let up = grille[posBombTop - 1][posBombLeft];
+    let right = grille[posBombTop][posBombLeft + 1];
+    let left = grille[posBombTop][posBombLeft - 1];
     let explosion;
 
+    // DOWN
+    if (down != 1 ) {
+        if (down != 3 ){
+
+            explosion = document.createElement("div");
+            explosion.setAttribute("class", "explosion");
+            map.appendChild(explosion);
+            explosion.style.top = (posBombTop + 1) * 50 + "px";
+            explosion.style.left = posBombLeft * 50 + "px";
+            bomb.style.backgroundImage = "url('./images/explo.png')";
+            explosion.style.backgroundImage = "url('./images/explo.png')";
+            grille[posBombTop + 1][posBombLeft] = 0;
+
+        /*killEnemy(explosion);
+        killHero(explosion);*/
+        breakBrick(explosion);
+
+        //UP
+    }} if (up != 1 )  {
+        if (up != 3 ){
+
+            explosion = document.createElement("div");
+            explosion.setAttribute("class", "explosion");
+            map.appendChild(explosion);
+            explosion.style.top = (posBombTop - 1) * 50 + "px";
+            explosion.style.left = posBombLeft * 50 + "px";
+            bomb.style.backgroundImage = "url('./images/explo.png')";
+            explosion.style.backgroundImage = "url('./images/explo.png')";
+            grille[posBombTop - 1][posBombLeft] = 0;
+
+        /*killEnemy(explosion);
+        killHero(explosion);*/
+        breakBrick(explosion);
+
+        //RIGHT
+    }} if (right != 1  )  {
+        if (right != 3  ){
+
+            explosion = document.createElement("div");
+            explosion.setAttribute("class", "explosion");
+            map.appendChild(explosion);
+            explosion.style.top = posBombTop * 50 + "px";
+            explosion.style.left = (posBombLeft + 1) * 50 + "px";
+            bomb.style.backgroundImage = "url('./images/explo.png')";
+            explosion.style.backgroundImage = "url('./images/explo.png')";
+            grille[posBombTop][posBombLeft + 1]= 0;
+
+
+        /*killEnemy(explosion);
+        killHero(explosion);*/
+        breakBrick(explosion);
+
+        //LEFT
+    }} if (left != 1  )  {
+     if (left != 3 ){
 
         explosion = document.createElement("div");
         explosion.setAttribute("class", "explosion");
         map.appendChild(explosion);
-        explosion.style.top = (posBombTop + 1) * 50 + "px";
-        explosion.style.left = posBombLeft * 50 + "px";
+        explosion.style.top = posBombTop * 50 + "px";
+        explosion.style.left = (posBombLeft - 1) * 50 + "px";
         bomb.style.backgroundImage = "url('./images/explo.png')";
         explosion.style.backgroundImage = "url('./images/explo.png')";
-        
+        grille[posBombTop][posBombLeft - 1] = 0;
+
 
         /*killEnemy(explosion);
-        killHero(explosion);
-        killBrick(explosion);*/
-
+        killHero(explosion);*/
+        breakBrick(explosion);
+    }}
 }
 
+// remplace le background par floor si explosion sur brick
+function breakBrick(explosion){
+ var brick = document.getElementsByClassName('brick');
 
+    for (var i = brick.length - 1; i >= 0; i--) {
+    var brickLeft = brick[i].offsetLeft / 50;
+    var brickTop = brick[i].offsetTop / 50;
+
+        if ((explosion.offsetTop / 50 == brickTop) && (explosion.offsetLeft / 50 == brickLeft)) {
+            brick[i].style.backgroundImage= "url('./images/floor.png')";
+        }
+    }  
+}
 
